4036a300d826f72e8fe3ca385378f8a12f83f4dd
Mubarak Hossain <mubarakhossain14@gmail.com>
2023-11-05 15:11:04 +0600

    Github link added

--- resources/views/pattern-matching.blade.php
+++ resources/views/pattern-matching.blade.php
@@ -19,12 +19,20 @@
     <main class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
             <h1 class="text-4xl font-bold tracking-tight text-gray-900">Pattern Matching</h1>
-            <a class="inline-flex justify-center rounded-lg text-sm font-semibold py-2 px-4 bg-slate-900 text-white hover:bg-slate-700" href="{{ route('home') }}">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-                </svg>
-                Home
-            </a>
+            <div>
+                <a class="inline-flex justify-center rounded-lg text-sm font-semibold py-2 px-4 bg-slate-900 text-white hover:bg-slate-700" href="{{ route('home') }}">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+                    </svg>
+                    Home
+                </a>
+                <a target="_blank" class="inline-flex justify-center rounded-lg text-sm font-semibold py-2 px-4 bg-slate-900 text-white hover:bg-slate-700" href="https://github.com/mubarakhossain14/nagadhat-tasks/commit/3ead406d92f1002ace4c49c866de6970eadc577d">
+                    View in GitHub
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 ml-2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+                    </svg>
+                </a>
+            </div>
         </div>
 
         <section class="pb-24 pt-6">
